7251cf81b3ce1cca5cc1f8420d253130e84db904
Aisling Fontaine <aisling.fontaine@protonmail.com>
2026-02-17 13:21:24 +0000

    ConMan: Add draft of Request interface

--- include/Request.hpp
+++ include/Request.hpp
@@ -1,12 +1,31 @@
 #ifndef REQUEST_HPP
 #define REQUEST_HPP
 
-class Request {
-    private:
-    public:
-        // parse();
-        // get_content();
-        // launch_cgi
+#include "HTTP_Message.hpp"
+#include "config.hpp"
+
+enum HTTP_Method { GET, POST, DELETE };
+enum Status_Parsing {
+    EMPTY,
+    REQUEST_LINE,
+    HEADERS,
+    BODY,
+    PARSED
+};  // TODO Might move it to a parsing-related header if needed elsewhere
+
+/**
+ * @brief Represents a request issued by an active connection.
+ */
+class Request : virtual public HTTP_Message {
+public:
+    Request();
+    ~Request();
+
+private:
+    HTTP_Method    _method;
+    Status_Parsing _status;
+
+    const Config_Location& _config;
 };
 
 #endif
